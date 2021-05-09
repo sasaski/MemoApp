114414796b22cacc27f1b2bce03d21ab2c278434
sasakigoma <sasaki.goma4444@gmail.com>
2021-05-09 23:39:01 +0900

    create MemoList MemoDetail Screen

--- App.jsx
+++ App.jsx
@@ -1,53 +1,13 @@
+/* eslint-disable-next-line */
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
 
-import AppBar from './src/components/AppBar';
-import MemoList from './src/components/MemoList';
-import CircleButton from './src/components/CircleButton';
+import MemoListScreen from './src/screens/MemoListScreen';
+import MemoDetailScreen from './src/screens/MemoDetailScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      {/** memoAppView */}
-      {/** AppBar */}
-      <AppBar />
-      {/** memoListView */}
-      <MemoList />
-      {/** createButtonView */}
-      <CircleButton>+</CircleButton>
-      {/* eslint-disable-next-line */}
-      <StatusBar style="auto" />
-    </View>
+    // <MemoListScreen />
+    <MemoDetailScreen />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F0F4F8',
-  },
-  circleButton: {
-    backgroundColor: '#467FD3',
-    width: 64,
-    height: 64,
-    borderRadius: 32,
-    justifyContent: 'center',
-    alignItems: 'center',
-    position: 'absolute',
-    right: 40,
-    bottom: 40,
-    /** ios shadow */
-    shadowColor: '#000000',
-    shadowOffset: { width: 0, height: 8 },
-    shadowOpacity: 0.25,
-    shadowRadius: 8,
-    /** Android shadow */
-    elevation: 8,
-  },
-  circleButtonLabal: {
-    color: '#FFFFFF',
-    fontSize: 40,
-    lineHeight: 40,
-  },
-});
---
+++ src/screens/MemoDetailScreen.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import {
+  View,
+  ScrollView,
+  Text,
+  StyleSheet,
+} from 'react-native';
+
+import AppBar from '../components/AppBar';
+import CircleButton from '../components/CircleButton';
+
+export default function MemoDetailScreen() {
+  return (
+    <View style={styles.container}>
+      {/* AppBar */}
+      <AppBar />
+      {/** memoHeaderView */}
+      <View style={styles.memoHeader}>
+        <Text style={styles.memoTitle}>買い物リスト</Text>
+        <Text style={styles.memoDate}>2020年12月24日 10:00</Text>
+      </View>
+      {/** memoContents */}
+      <ScrollView style={styles.memoContents}>
+        <Text style={styles.memoText}>
+          買い物リスト書体やレイアウトなどを確認するために用います。本文用なので使い方を間違えると不自然に見えることもありますので要注意。
+
+          カタカナ語が苦手な方は「組見本」と呼ぶとよいでしょう。なお、組見本の「組」とは文字組のことです。活字印刷時代の用語だったと思います。
+          このダミーテキストは自由に改変することが出来ます。主に書籍やウェブページなどのデザインを作成する時によく使われます。書体やレイアウトなどを確認するために用います。
+
+          ダミーテキストはダミー文書やダミー文章とも呼ばれることがあります。カタカナ語が苦手な方は「組見本」と呼ぶとよいでしょう。
+          主に書籍やウェブページなどのデザインを作成する時によく使われます。これは正式な文章の代わりに入れて使うダミーテキストです。
+          買い物リスト書体やレイアウトなどを確認するために用います。本文用なので使い方を間違えると不自然に見えることもありますので要注意。
+
+          カタカナ語が苦手な方は「組見本」と呼ぶとよいでしょう。なお、組見本の「組」とは文字組のことです。
+          活字印刷時代の用語だったと思います。このダミーテキストは自由に改変することが出来ます。主に書籍やウェブページなどのデザインを作成する時によく使われます。
+          書体やレイアウトなどを確認するために用います。
+
+          ダミーテキストはダミー文書やダミー文章とも呼ばれることがあります。カタカナ語が苦手な方は「組見本」と呼ぶとよいでしょう。主に書籍やウェブページなどのデザインを作成する時によく
+        </Text>
+      </ScrollView>
+
+      {/** CircleButton */}
+      <CircleButton style={{ top: 160, bottom: 'auto' }}>+</CircleButton>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+  },
+  memoHeader: {
+    backgroundColor: '#467FD3',
+    height: 96,
+    paddingHorizontal: 19,
+    paddingVertical: 24,
+    justifyContent: 'center',
+  },
+  memoTitle: {
+    color: '#FFFFFF',
+    fontSize: 20,
+    lineHeight: 32,
+    fontWeight: 'bold',
+  },
+  memoDate: {
+    color: '#FFFFFF',
+    fontSize: 12,
+    lineHeight: 16,
+  },
+  memoContents: {
+    paddingVertical: 27,
+    paddingHorizontal: 32,
+  },
+  memoText: {
+    fontSize: 16,
+    lineHeight: 24,
+  },
+});
