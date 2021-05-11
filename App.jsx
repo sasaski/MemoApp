e18bf054b9a90b8cfb6e88aba515b6c6a113d3e7
sasakigoma <sasaki.goma4444@gmail.com>
2021-05-11 23:34:34 +0900

    create MemoEditScreen and MemoCreateScreen

--- App.jsx
+++ App.jsx
@@ -2,12 +2,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 
-import MemoListScreen from './src/screens/MemoListScreen';
+// import MemoListScreen from './src/screens/MemoListScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
+// import MemoEditScreen from './src/screens/MemoEditScreen';
+import MemoCreateScreen from './src/screens/MemoCreateScreen';
 
 export default function App() {
   return (
-    <MemoListScreen />
+    // <MemoListScreen />
     // <MemoDetailScreen />
+    // <MemoEditScreen />
+    <MemoCreateScreen />
   );
 }
---
+++ src/screens/MemoEditScreen.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {
+  View, TextInput, StyleSheet, KeyboardAvoidingView,
+} from 'react-native';
+
+import AppBar from '../components/AppBar';
+import CircleButton from '../components/CircleButton';
+
+export default function MemoEditScreen() {
+  return (
+    <KeyboardAvoidingView style={styles.container} behavior="height">
+      <AppBar />
+      <View style={styles.inputContainer}>
+        {/** multilineは複数行入力を許可するためのプロパティ */}
+        <TextInput value="買い物リスト" multiline style={styles.input} />
+      </View>
+      <CircleButton name="check" />
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  inputContainer: {
+    paddingVertical: 32,
+    paddingHorizontal: 27,
+    flex: 1,
+  },
+  input: {
+    fontSize: 16,
+    lineHeight: 24,
+    flex: 1,
+    // Androidがdefualtがcenterのため、明示的に指定する
+    textAlignVertical: 'top',
+  },
+});
