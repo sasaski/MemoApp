7e26a07fde18f410ce152ca7bce7e6bd39c82aea
sasakigoma <sasaki.goma4444@gmail.com>
2021-05-24 00:13:09 +0900

    Update memo data on MemoEditScreen

--- src/screens/MemoEditScreen.jsx
+++ src/screens/MemoEditScreen.jsx
@@ -1,23 +1,61 @@
-import React from 'react';
+import { shape, string } from 'prop-types';
+import React, { useState } from 'react';
 import {
-  View, TextInput, StyleSheet,
+  View, TextInput, StyleSheet, Alert,
 } from 'react-native';
+import firebase from 'firebase';
 
 import CircleButton from '../components/CircleButton';
 import KeyboardSafeView from '../components/KeyboardSafeView';
 
 export default function MemoEditScreen(props) {
-  const { navigation } = props;
+  const { navigation, route } = props;
+  const { id, bodyText } = route.params;
+  const [body, setbody] = useState(bodyText);
+
+  function handlePress() {
+    const { currentUser } = firebase.auth();
+    if (currentUser) {
+      const db = firebase.firestore();
+      const ref = db.collection(`users/${currentUser.uid}/memos`).doc(id);
+      ref.set({
+        bodyText: body,
+        updatedAt: new Date(),
+      })
+        .then(() => {
+          navigation.goBack();
+        })
+        .catch((error) => {
+          Alert.alert(error.code);
+        });
+    }
+  }
+
   return (
     <KeyboardSafeView style={styles.container}>
       <View style={styles.inputContainer}>
         {/** multilineは複数行入力を許可するためのプロパティ */}
-        <TextInput value="買い物リスト" multiline style={styles.input} />
+        <TextInput
+          value={body}
+          multiline
+          style={styles.input}
+          // 変更用のファンクション。文字入力用
+          onChangeText={(text) => { setbody(text); }}
+        />
       </View>
-      <CircleButton name="check" onPress={() => { navigation.goBack(); }} />
+      <CircleButton name="check" onPress={handlePress} />
     </KeyboardSafeView>
   );
 }
+
+MemoEditScreen.propTypes = {
+  route: shape({
+    params: shape({
+      id: string,
+      bodyText: string,
+    }),
+  }).isRequired,
+};
 
 const styles = StyleSheet.create({
   container: {
